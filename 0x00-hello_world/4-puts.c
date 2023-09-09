19c6eb2a1515b453601f3f3300c7c380a3319b73
selma <selmaabdelhakk@gmail.com>
2023-09-09 09:54:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main -A program that prints a line with puts function
+ * RE=eturn 0(success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0)
+}
