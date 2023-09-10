e3c84247cedb1739249313f90029111b728090a1
selma <selmaabdelhakk@gmail.com>
2023-09-10 02:18:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
