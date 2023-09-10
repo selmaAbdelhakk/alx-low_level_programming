3ca09fc6a62d7541f8510ccfeeda3e16d680350f
selma <selmaabdelhakk@gmail.com>
2023-09-10 02:09:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0(success)
+*/
+
+int main(void)
+{
+	char = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
+
