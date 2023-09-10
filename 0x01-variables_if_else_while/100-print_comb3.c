cf88b2e3b6e52eb7e963ca591a5e2306a371f382
selma <selmaabdelhakk@gmail.com>
2023-09-10 03:11:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,20 +16,20 @@ int main(void)
 		{
 			if (digit1 != digit2 && digit1 < digit2)
 			{
-			putchar(digit1 + 48);
-		        putchar(digit2 + 48);
+			       putchar(digit1 + 48);
+		               putchar(digit2 + 48);
 		        
-		        if (digit1 + digit2 != 17)
-			{
-			        putchar(',');
-			        putchar(' ');
+		               if (digit1 + digit2 != 17)
+			       {
+			              putchar(',');
+			              putchar(' ');
+			       }
 			}
-			}
-		digit2++;
+		        digit2++;
 		}
-	digit1++;
+         	digit1++;
 	}
-putchar('\');
+        putchar('\');
 
-return (0);
+        return (0);
 }
