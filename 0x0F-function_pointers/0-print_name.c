8c0a040775378e3796c13b45db3b43383dbb1a85
selma <selmaabdelhakk@gmail.com>
2023-10-12 09:38:30 -0700

    1-array_iterator

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
- * Return: nothing
+ * Return: 
  **/
 void print_name(char *name, void (*f)(char *))
 	{
